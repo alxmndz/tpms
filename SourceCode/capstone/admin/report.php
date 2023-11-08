598d12422f245f5d478b9e583ada25e18f7b7dbc
alxmndz <xanderm854@gmail.com>
2023-11-09 01:45:10 +0800

    aditional chart

--- SourceCode/capstone/admin/report.php
+++ SourceCode/capstone/admin/report.php
@@ -12,12 +12,19 @@
 <?php 
   $con = new mysqli('localhost', 'root', '', 'test');
 
-  $query1 = "SELECT COUNT(*) AS count FROM baptismal_tbl";
-	$query2 = "SELECT COUNT(*) AS count FROM blessing_tbl";
-	$query3 = "SELECT COUNT(*) AS count FROM communion_tbl";
-	$query4 = "SELECT COUNT(*) AS count FROM confirmation_tbl";
-	$query5 = "SELECT COUNT(*) AS count FROM funeralmass_tbl";
-	$query6 = "SELECT COUNT(*) AS count FROM wedding_tbl";
+  $query1 = "SELECT COUNT(*) AS count FROM baptismal_tbl WHERE transactType = 'Walk-In'";
+	$query2 = "SELECT COUNT(*) AS count FROM blessing_tbl WHERE transactType = 'Walk-In'";
+	$query3 = "SELECT COUNT(*) AS count FROM communion_tbl WHERE transactType = 'Walk-In'";
+	$query4 = "SELECT COUNT(*) AS count FROM confirmation_tbl WHERE transactType = 'Walk-In'";
+	$query5 = "SELECT COUNT(*) AS count FROM funeralmass_tbl WHERE transactType = 'Walk-In'";
+	$query6 = "SELECT COUNT(*) AS count FROM wedding_tbl WHERE transactType = 'Walk-In'";
+
+	$query1 = "SELECT COUNT(*) AS count FROM baptismal_tbl WHERE transactType = 'Walk-In'";
+	$query2 = "SELECT COUNT(*) AS count FROM blessing_tbl WHERE transactType = 'Walk-In'";
+	$query3 = "SELECT COUNT(*) AS count FROM communion_tbl WHERE transactType = 'Walk-In'";
+	$query4 = "SELECT COUNT(*) AS count FROM confirmation_tbl WHERE transactType = 'Walk-In'";
+	$query5 = "SELECT COUNT(*) AS count FROM funeralmass_tbl WHERE transactType = 'Walk-In'";
+	$query6 = "SELECT COUNT(*) AS count FROM wedding_tbl WHERE transactType = 'Walk-In'";
 
   $result1 = mysqli_query($conn, $query1);
 	$result2 = mysqli_query($conn, $query2);
@@ -37,10 +44,16 @@
 
 ?>
 
-
 <div style="width: 500px;">
   <canvas id="chart"></canvas>
 </div>
+
+			<div> 
+         <canvas id="layanan" width="240px" height="240px"></canvas> 
+      </div> 
+      <div> 
+         <canvas id="layanan_subbagian" width="240px" height="240px"></canvas> 
+      </div>  
  
 <script>
   const labels = ['Baptismal', 'Blessing', 'Communion', 'Confirmation' ,'Funeral', 'Wedding'];
@@ -74,6 +87,71 @@
     config
   );
 </script>
+<script>
+        $(function () {/*from   w ww .  ja va2 s  . c o  m*/
+            var ctx = document.getElementById("layanan").getContext('2d');
+            var data = {
+                datasets: [{
+                    data: [10, 20, 30],
+                    backgroundColor: [
+                        '#3c8dbc',
+                        '#f56954',
+                        '#f39c12',
+                    ],
+                }],
+                labels: [
+                    'Request',
+                    'Layanan',
+                    'Problem'
+                ]
+            };
+            var myDoughnutChart = new Chart(ctx, {
+                type: 'doughnut',
+                data: data,
+                options: {
+                    responsive: false,
+                    maintainAspectRatio: false,
+                    legend: {
+                        position: 'bottom',
+                        labels: {
+                            boxWidth: 12
+                        }
+                    }
+                }
+            });
+            var ctx_2 = document.getElementById("layanan_subbagian").getContext('2d');
+            var data_2 = {
+                datasets: [{
+                    data: [10, 20, 30],
+                    backgroundColor: [
+                        '#3c8dbc',
+                        '#f56954',
+                        '#f39c12',
+                    ],
+                }],
+                labels: [
+                    'Request',
+                    'Layanan',
+                    'Problem'
+                ]
+            };
+            var myDoughnutChart_2 = new Chart(ctx_2, {
+                type: 'doughnut',
+                data: data_2,
+                options: {
+                    responsive: false,
+                    maintainAspectRatio: false,
+                    legend: {
+                        position: 'bottom',
+                        labels: {
+                            boxWidth: 12
+                        }
+                    }
+                }
+            });
+        });
+    
+      </script> 
 
 
 </body>
